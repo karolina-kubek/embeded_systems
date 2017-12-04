b72eb034498d365f02a13b774117e433e837a46e
karolina-kubek <kubek.karolina@gmail.com>
2017-12-04 00:08:26 +0000

    Added RSSI and LQI

--- example-broadcast.c
+++ example-broadcast.c
@@ -1,15 +1,34 @@
 #include "contiki.h"
 #include "net/rime.h"
 #include "random.h"
-
 #include "dev/button-sensor.h"
-
 #include "dev/leds.h"
-                  
 #include "dev/sht11-sensor.h"
-
 #include <math.h>
 #include <stdio.h>
+#include "lib/list.h"
+
+/*struct RSSI_LQI_list_struct {
+	struct *next;
+	int addr1;
+	int addr2;
+	int rssi;
+	int lqi;
+};
+*/
+
+//LIST(example_list);
+
+//static struct RSSI_LQI_list_struct element1, element2;
+
+//static struct etimer et;
+static int val;
+static double s = 0;
+static int n = 0;
+static double temp_avg = 0; 
+static double humid_avg = 0;
+static int rssi = 0;
+static int lqi = 0;
 
 //Each mote sends periodically a broadcast. However, it does not send any broadcast message until the user click the button.
 /*---------------------------------------------------------------------------*/
@@ -18,88 +37,67 @@ AUTOSTART_PROCESSES(&example_broadcast_process);
 /*---------------------------------------------------------------------------*/
 static void
 broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
-{
-  printf("broadcast message received from %d.%d: '%s'\n",
-         from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+{	
+	rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+	lqi = packetbuf_attr(PACKETBUF_ATTR_LINK_QUALITY);
+	printf("broadcast message received from %d.%d: '%s' RSSI: '%d' LQI: '%d'\n",
+	from->u8[0], from->u8[1], (char *)packetbuf_dataptr()), rssi, lqi;
+
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(example_broadcast_process, ev, data)
 {
-  static struct etimer et;
+	static struct etimer et;
+	PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+	PROCESS_BEGIN();
+	printf("Click the button to start\n");
+	SENSORS_ACTIVATE(button_sensor);
+	PROCESS_WAIT_EVENT_UNTIL((ev==sensors_event) && (data == &button_sensor));
+	printf("Broadcast starting...\n");
+	broadcast_open(&broadcast, 129, &broadcast_call);
+	while(1) {
+		/* Delay 2-4 seconds */
+		etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+		
+		
+		// compute_averages();
+		while( n < 5 )
+		{
+			etimer_set(&et, CLOCK_SECOND * 2);
+			SENSORS_ACTIVATE(sht11_sensor);
+			PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+			val = sht11_sensor.value(SHT11_SENSOR_TEMP);
+			if(val != -1)
+			{
+				s= ((0.01*val) - 39.60);
+				temp_avg = temp_avg + s;
 
-  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
-
-  PROCESS_BEGIN();
-  printf("Click the button to start\n");
-  SENSORS_ACTIVATE(button_sensor);
-  PROCESS_WAIT_EVENT_UNTIL((ev==sensors_event) && (data == &button_sensor));
-  printf("Broadcast starting...\n");
-  broadcast_open(&broadcast, 129, &broadcast_call);
-
-  while(1) {
-
-    /* Delay 2-4 seconds */
-    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
-
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-
-    packetbuf_copyfrom("Karolina", 9);
-    broadcast_send(&broadcast);
-    printf("broadcast message sent\n");
-  }
-
-  PROCESS_END();
+			}
+			val=sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
+			if(val != -1)
+			{
+				s= (((0.0405*val) - 4) + ((-2.8 * 0.000001)*(pow(val,2))));
+				humid_avg = humid_avg + s;
+			}
+			etimer_reset(&et);
+			SENSORS_DEACTIVATE(sht11_sensor);
+	
+			n = n + 1;
+		}
+		n = 0;
+		temp_avg = temp_avg / 5;
+		printf("\nTemperature=%lf C \n", temp_avg);
+		humid_avg = humid_avg / 5;
+		printf("Humidity=%lf %% \n", humid_avg);
+		
+		packetbuf_copyfrom("Bean", 5);
+		broadcast_send(&broadcast);
+		printf("broadcast message sent\n");
+	}
+	PROCESS_END();
 }
 
-//Each mote periodically samples temperature and humidity and computes its average over 5 samples.
-PROCESS(sensor_acq_process,"Sensor Acquisition");
-AUTOSTART_PROCESSES(&sensor_acq_process);
-PROCESS_THREAD(sensor_acq_process,ev,data)
-{ 
-      static struct etimer et;
-      static int val;
-      static float s = 0;
-      static int dec;
-      static float frac;
-
-      PROCESS_BEGIN();
-
-      printf("Starting Sensor Example.\n");
-      
-      while(1)
-      {
-		etimer_set(&et, CLOCK_SECOND * 5);
-		SENSORS_ACTIVATE(sht11_sensor);
-        
-	   PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-
-
-           val = sht11_sensor.value(SHT11_SENSOR_TEMP);
-      	   if(val != -1) 
-      	   {
-			s= ((0.01*val) - 39.60);
-      	  	dec = s;
-      	  	frac = s - dec;
-      	  	printf("\nTemperature=%d.%02u C (%d)\n", dec, (unsigned int)(frac * 100),val);               
-           }
-
-	   val=sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
-	   if(val != -1) 
-      	   {
-			s= (((0.0405*val) - 4) + ((-2.8 * 0.000001)*(pow(val,2))));  
-      	  	dec = s;
-      	  	frac = s - dec;
-      	  	printf("Humidity=%d.%02u %% (%d)\n", dec, (unsigned int)(frac * 100),val);               
-           }
-
-           
-	
-	   etimer_reset(&et);
-    	   SENSORS_DEACTIVATE(sht11_sensor);
-
-      } //end of while
-    
-      PROCESS_END();
 /*---------------------------------------------------------------------------*/
