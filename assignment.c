43e43ed6e7acfaecd9c6df94a86425ac98acd27c
karolina-kubek <kubek.karolina@gmail.com>
2017-12-07 08:35:53 +0000

    Points 1 to 5 done

---
+++ assignment.c
@@ -0,0 +1,281 @@
+#include "contiki.h"
+#include "lib/list.h"
+#include "lib/memb.h"
+#include "lib/random.h"
+#include "net/rime.h"
+#include "dev/button-sensor.h"
+#include "dev/leds.h"
+#include "dev/sht11-sensor.h"
+#include <math.h>
+#include <stdio.h>
+
+static int val;
+static float s = 0;
+static float temp_avg = 0; 
+static float humid_avg = 0;
+
+/* This is the structure of broadcast messages. */
+struct broadcast_message {
+  uint8_t seqno;
+};
+
+/* This is the structure of unicast ping messages. */
+struct unicast_message {
+
+ int temp_dec;
+ float temp_frac;
+ int humid_dec;
+ float humid_frac;
+ 
+};
+
+
+/* This structure holds information about neighbors. */
+struct neighbor {
+
+  struct neighbor *next;
+  rimeaddr_t addr;
+  uint16_t last_rssi, last_lqi;
+  uint8_t last_seqno;
+  uint32_t avg_seqno_gap;
+
+};
+
+/* This #define defines the maximum amount of neighbors we can remember. */
+#define MAX_NEIGHBORS 16
+
+/* This MEMB() definition defines a memory pool from which we allocate
+   neighbor entries. */
+MEMB(neighbors_memb, struct neighbor, MAX_NEIGHBORS);
+
+/* The neighbors_list is a Contiki list that holds the neighbors we
+   have seen thus far. */
+LIST(neighbors_list);
+
+/* These hold the broadcast and unicast structures, respectively. */
+static struct broadcast_conn broadcast;
+static struct unicast_conn unicast;
+
+/* These two defines are used for computing the moving average for the
+   broadcast sequence number gaps. */
+#define SEQNO_EWMA_UNITY 0x100
+#define SEQNO_EWMA_ALPHA 0x040
+
+/*---------------------------------------------------------------------------*/
+/* We first declare our two processes. */
+PROCESS(broadcast_process, "Broadcast process");
+PROCESS(unicast_process, "Unicast process");
+
+/* The AUTOSTART_PROCESSES() definition specifices what processes to
+   start when this module is loaded. We put both our processes
+   there. */
+AUTOSTART_PROCESSES(&broadcast_process, &unicast_process);
+/*---------------------------------------------------------------------------*/
+/* This function is called whenever a broadcast message is received. */
+static void
+broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
+{
+  struct neighbor *n;
+  struct broadcast_message *m;
+  uint8_t seqno_gap;
+
+  /* The packetbuf_dataptr() returns a pointer to the first data byte
+     in the received packet. */
+  m = packetbuf_dataptr();
+
+  /* Check if we already know this neighbor. */
+  for(n = list_head(neighbors_list); n != NULL; n = list_item_next(n)) {
+
+    /* We break out of the loop if the address of the neighbor matches
+       the address of the neighbor from which we received this
+       broadcast message. */
+    if(rimeaddr_cmp(&n->addr, from)) {
+      break;
+    }
+  }
+
+  /* If n is NULL, this neighbor was not found in our list, and we
+     allocate a new struct neighbor from the neighbors_memb memory
+     pool. */
+  if(n == NULL) {
+    n = memb_alloc(&neighbors_memb);
+
+    /* If we could not allocate a new neighbor entry, we give up. We
+       could have reused an old neighbor entry, but we do not do this
+       for now. */
+    if(n == NULL) {
+      return;
+    }
+
+    /* Initialize the fields. */
+    rimeaddr_copy(&n->addr, from);
+    n->last_seqno = m->seqno - 1;
+    n->avg_seqno_gap = SEQNO_EWMA_UNITY;
+
+    /* Place the neighbor on the neighbor list. */
+    list_add(neighbors_list, n);
+  }
+
+  /* We can now fill in the fields in our neighbor entry. */
+  n->last_rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+  n->last_lqi = packetbuf_attr(PACKETBUF_ATTR_LINK_QUALITY);
+
+  /* Compute the average sequence number gap we have seen from this neighbor. */
+  seqno_gap = m->seqno - n->last_seqno;
+  n->avg_seqno_gap = (((uint32_t)seqno_gap * SEQNO_EWMA_UNITY) *
+                      SEQNO_EWMA_ALPHA) / SEQNO_EWMA_UNITY +
+                      ((uint32_t)n->avg_seqno_gap * (SEQNO_EWMA_UNITY -
+                                                     SEQNO_EWMA_ALPHA)) /
+    SEQNO_EWMA_UNITY;
+
+  /* Remember last seqno we heard. */
+  n->last_seqno = m->seqno;
+
+  /* Print out a message. */
+  printf("broadcast message received from %d.%d with seqno %d, RSSI %u, LQI %u, avg seqno gap %d.%02d\n",
+         from->u8[0], from->u8[1],
+         m->seqno,
+         packetbuf_attr(PACKETBUF_ATTR_RSSI),
+         packetbuf_attr(PACKETBUF_ATTR_LINK_QUALITY),
+         (int)(n->avg_seqno_gap / SEQNO_EWMA_UNITY),
+         (int)(((100UL * n->avg_seqno_gap) / SEQNO_EWMA_UNITY) % 100));
+}
+/* This is where we define what function to be called when a broadcast
+   is received. We pass a pointer to this structure in the
+   broadcast_open() call below. */
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+/*---------------------------------------------------------------------------*/
+/* This function is called for every incoming unicast packet. */
+static void
+recv_uc(struct unicast_conn *c, const rimeaddr_t *from)
+{
+  /*struct unicast_message *msg;
+
+   Grab the pointer to the incoming data. 
+  msg = packetbuf_dataptr();*/
+
+  printf("unicast message received from %d.%d\n", from->u8[0], from->u8[1]);
+  
+}
+static const struct unicast_callbacks unicast_callbacks = {recv_uc};
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(broadcast_process, ev, data)
+{
+  static struct etimer et;
+  static uint8_t seqno;
+  struct broadcast_message msg;
+
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+
+  PROCESS_BEGIN();
+
+  /*Each mote sends periodically a broadcast. However, it does not send any broadcast 			
+	  message until the user click the button.*/
+	printf("Click the button to start\n");
+	SENSORS_ACTIVATE(button_sensor);
+	PROCESS_WAIT_EVENT_UNTIL((ev==sensors_event) && (data == &button_sensor));
+	printf("Broadcast starting...\n");
+
+  broadcast_open(&broadcast, 129, &broadcast_call);
+
+  while(1) {
+
+    /* Send a broadcast every 4 - 8 seconds */
+    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+    msg.seqno = seqno;
+    packetbuf_copyfrom(&msg, sizeof(struct broadcast_message));
+    broadcast_send(&broadcast);
+    seqno++;
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(unicast_process, ev, data)
+{
+  PROCESS_EXITHANDLER(unicast_close(&unicast);)
+    
+  PROCESS_BEGIN();
+
+  unicast_open(&unicast, 146, &unicast_callbacks);
+
+  while(1) {
+    static struct etimer et;
+    struct unicast_message msg;
+    struct neighbor *n;
+    int maxneighbor;
+    int i = 0;
+    uint16_t max_rssi = 0;
+    short k = 0;
+    
+		/******************************************************************************/
+	  // Each mote periodically samples temperature and humidity and computes its average 
+    // over 5 samples.
+		while( k < 5 )
+		{
+			etimer_set(&et, CLOCK_SECOND * 5);
+			PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+			
+			SENSORS_ACTIVATE(sht11_sensor);
+			val = sht11_sensor.value(SHT11_SENSOR_TEMP);
+			
+			if(val != -1)
+			{
+				s= ((0.01*val) - 39.60);
+				temp_avg = temp_avg + s;
+
+			}
+			val=sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
+			
+			if(val != -1)
+			{
+				s= (((0.0405*val) - 4) + ((-2.8 * 0.000001)*(pow(val,2))));
+				humid_avg = humid_avg + s;
+			}
+			
+			etimer_reset(&et);
+			SENSORS_DEACTIVATE(sht11_sensor);
+	
+			k = k + 1;
+		}
+		
+		temp_avg = temp_avg / 5;
+		msg.temp_dec = temp_avg;
+    msg.temp_frac = temp_avg - msg.temp_dec;
+		printf("\nTemperature=%d.%02u C \n", msg.temp_dec, (unsigned int)(msg.temp_frac * 100));
+		humid_avg = humid_avg / 5;
+		msg.humid_dec = humid_avg;
+    msg.humid_frac = humid_avg - msg.humid_dec;
+		printf("Humidity=%d.%02u %% \n", msg.humid_dec, (unsigned int)(msg.humid_frac * 100));
+    /********************************************************************/
+
+		/*Each mote sends its sample to its neighbour with the highest RSSI value*/
+		if(list_length(neighbors_list) > 0) {
+		
+			for(n = list_head(neighbors_list); n != NULL; n = list_item_next(n)) {
+				printf("max rssi: %u", max_rssi);
+				printf("max neighbour num: '%d", maxneighbor);
+				if (n->last_rssi > max_rssi) {
+					max_rssi = n->last_rssi;
+					maxneighbor = i;
+				}
+				i = i + 1;
+			}
+			
+			n = list_head(neighbors_list);
+      for(i = 0; i < maxneighbor; i++) {
+        n = list_item_next(n);
+      }
+      
+      printf("sending unicast to %d.%d\n", n->addr.u8[0], n->addr.u8[1]);
+      packetbuf_copyfrom(&msg, sizeof(msg));
+      unicast_send(&unicast, &n->addr);
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
